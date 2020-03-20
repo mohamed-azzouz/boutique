a2cde3899d13b08e2eee2196b1ca07b0d7b8b512
mohamed-azzouz <mohamed.azzouz@laplateforme.io>
2020-03-20 06:01:02 +0100

    Page connexion vierge

---
+++ connexion.php
@@ -0,0 +1,28 @@
+<?php
+	session_start();
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Page Connexion</title>
+	<link rel="stylesheet" type="text/css" href="boutique.css">
+</head>
+<body>
+	HEADER
+
+	<main>
+
+		<form>
+			Login : <input type="text" name="login"> <br />
+			Password : <input type="password" name="password"> <br />
+		</form>
+		
+	</main>
+
+	FOOTER
+
+</body>
+</html>
