04d465bec12d9bca643df51dd24bd07e277f1946
paul-le <paulloukyle@gmail.com>
2020-03-20 16:26:28 +0100

    Avancement de projet au niveau du CSS

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <section id="sectionTopHeader">
+            <h1 id="headerH1">Welcome</h1>
+        </section>
+        <section id="sectionBottomHeader">
+            <section id="imagesHeader">
+                <a href="index.php"><img id="bottomLogo" src="Images/logo.png"></a>
+                <a href="index.php"><img id="topLogo" src="Images/logov2.png"></a>
+            </section>
+            <section id="sectionBottomHeaderDroite">
+                <ul id="headerNavBar">
+                    <li>
+                        <img id="iconAccount" src="Images/iconacc.png">
+                        Mon compte
+                    </li>
+                    <li>
+                        <img id="iconAccount" src="Images/iconacc.png">
+                        Toast
+                    </li>
+                    
+                </ul>
+            </section>
+        </section>
+    </header>
+    <main>
+    <section id="flexFull">
+        <section id="partieGauche">
+        </section>
+            <section id="partieCentreTop">
+                <section id="testTop">
+                    <img id="cartIcon" src="Images/carticonpng.png">Mon panier
+                </section>
+                <section id="partieCentre">
+                    <section id="mainContent">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam obcaecati illo veniam! Illum adipisci architecto quo, doloribus voluptas non autem libero vitae, maxime, nobis excepturi aliquam quaerat optio laboriosam tempore!
+                    </section>
+                </section>
+            </section>
+        <section id="partieDroite">
+        </section>
+    </section>
+    </main>
+    <footer>
+    </footer>
+</body>
+</html>
