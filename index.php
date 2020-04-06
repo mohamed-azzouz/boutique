84ce017b3c2867961400edf4560cb8dcba267168
paul-le <paulloukyle@gmail.com>
2020-04-06 12:49:45 +0200

    Avancement du projet

--- index.php
+++ index.php
@@ -34,18 +34,39 @@
     <main>
     <section id="flexFull">
         <section id="partieGauche">
+            <!-- <img id="imgGaucheToast" src="Images/bannieregauche.jpg"> -->
         </section>
             <section id="partieCentreTop">
                 <section id="testTop">
-                    <img id="cartIcon" src="Images/carticonpng.png">Mon panier
+                    <img id="cartIcon" src="Images/carticonpng.png"> Mon panier
                 </section>
                 <section id="partieCentre">
+                    <img src="Images/banniere.jpg">
+                </section>
                     <section id="mainContent">
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam obcaecati illo veniam! Illum adipisci architecto quo, doloribus voluptas non autem libero vitae, maxime, nobis excepturi aliquam quaerat optio laboriosam tempore!
+                        <section id="mainContentFlexSeparation">
+                            <section id="mainTopContent">
+                                <section id="mainTopContentCategories">
+                                    DERNIERS COMMENTAIRES
+                                </section>
+                                <section id="mainTopContentListe">
+                                    DERNIERS JEUX AJOUTÉS
+                                </section>
+                                <section id="mainTopContentLastComment">
+                                    CATÉGORIES
+                                </section>
+                        </section>
+                        <section id="banniereSeparation">
+
+                            </section>
+                            <section id="meilleuresVentes">
+                                Meilleurs ventes
+                            </section>
+                        </section>
                     </section>
                 </section>
-            </section>
         <section id="partieDroite">
+            <!-- <img src="Images/bannieredroite.jpg"> -->
         </section>
     </section>
     </main>
