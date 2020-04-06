79af11c7718453efeac5ffcc3c5e1097e5c1a127
paul-le <paulloukyle@gmail.com>
2020-04-06 13:56:47 +0200

    Modifs & ajout footer

--- index.php
+++ index.php
@@ -71,6 +71,17 @@
     </section>
     </main>
     <footer>
+        <section id="footerMainSection">
+            <section id="footerFirstSection">
+                copyright Momohamed Paul
+            </section>
+            <section id="footerSecondSection">
+                Accueil Nous contacter FAQ
+            </section>
+            <section id="footerThirdSection">
+                <img src="Images/paiement.png">
+            </section>
+        </section>
     </footer>
 </body>
 </html>
